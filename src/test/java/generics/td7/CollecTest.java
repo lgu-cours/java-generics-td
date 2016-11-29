23ff980073c5bf060ddfb2b7fba87bef1af84164
Laurent Guerin <laguerin@LFR002634.corp.capgemini.com>
2016-11-29 11:47:11 +0100

    Tests moved in src/test

--- src/test/java/generics/td7/CollecTest.java
+++ src/test/java/generics/td7/CollecTest.java
@@ -1,31 +1,44 @@
 package generics.td7;
 
+import static org.junit.Assert.assertEquals;
 import generics.bean.Chien;
 
-public class TestCollec {
+import org.junit.Before;
+import org.junit.Test;
 
-	/**
-	 * @param args
-	 */
-	public static void main(String[] args) {
+public class CollecTest {
 
+	@Before
+	public void before() {
+		System.out.println("----------");
+	}
+	
+	@Test
+	public void test1() {
 		Integer ints[] = { 55, 11, 2, 8, 4, 12, 22 } ;
 		//Collec c = new Collec(ints); // Warning
 		Collec<Integer> c = new Collec<Integer>(ints);
 		System.out.println(" min : " + c.min() );
-		System.out.println(" max : " + c.max() );
-		
-		System.out.println("---");
-		
+		System.out.println(" max : " + c.max() );		
+		assertEquals(Integer.valueOf(2), c.min() );
+		assertEquals(Integer.valueOf(55), c.max() );
+	}
+
+	@Test
+	public void test2() {
 		String strings[] = { "b", "z", "c", "y", "a" } ;
 		//Collec c2 = new Collec(strings); // Warning
 		Collec<String> c2 = new Collec<String>(strings); 
 		System.out.println(" min : " + c2.min() );
 		System.out.println(" max : " + c2.max() );
-		
+		assertEquals("a", c2.min() );
+		assertEquals("z", c2.max() );
+	}
+
+	@Test
+	public void test3() {
 		Chien chiens[] = { new Chien("pif") } ;
 		//Collec<Chien> c3 = new Collec<Chien>(chiens); // ERR : Chien n'implémente pas "Comparable"
-		
 	}
 
 }
